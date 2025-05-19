96940ad9cec5541a71a5c68ef166859ec34f34e8
saedmanaf <saedmanaf@gmail.com>
2025-05-19 15:24:51 +0530

    updated test

--- app/src/androidTest/java/ai/elimu/maneno/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ai/elimu/maneno/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
-package org.literacyapp.maneno;
+package ai.elimu.maneno;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +19,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("org.literacyapp.missing_number", appContext.getPackageName());
+        assertEquals("ai.elimu.missing_number", appContext.getPackageName());
     }
 }
