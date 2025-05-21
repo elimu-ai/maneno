e474a5b96520d9d098c1e0091da960c2e8d27a6a
tuancoltech <tuancoltech@gmail.com>
2025-05-21 12:56:12 +0700

    Extract dynamic log tag to a global value in `MainActivity`

--- app/src/main/java/ai/elimu/maneno/MainActivity.kt
+++ app/src/main/java/ai/elimu/maneno/MainActivity.kt
@@ -6,14 +6,17 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.i(javaClass.getName(), "onCreate")
+        Log.i(TAG, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     override fun onStart() {
-        Log.i(javaClass.getName(), "onStart")
+        Log.i(TAG, "onStart")
         super.onStart()
 
         val intent = Intent(this, LetterSoundActivity::class.java)
